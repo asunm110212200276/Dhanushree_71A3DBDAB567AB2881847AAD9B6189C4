e85747f036cd5a4419392fd4e88583ba8b27471b
asunm110212200276 <145050039+asunm110212200276@users.noreply.github.com>
2023-09-29 18:07:35 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,10 +1,33 @@
-```python
-# Get input from the user
-year = int(input("Enter a year: "))
+#create an object of the both bowler and batsman classes and ca#Python program to create Bankaccount class with deposit, withdraw function.
+class Bank_Account:
 
-# Check if it's a leap year or not
-if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-    print(year, "is a leap year")
-else:
-    print(year, "is not a leap year")
-```
+  def __init__(self):
+    self.balance = 0
+    print("Account name=Tamil,Acc num = xxxxxxxxxxx4969")
+
+  def deposit(self):
+    amount = float(input("Enter amount to be Deposited: "))
+    self.balance += amount
+    print("\n Amount Deposited:", amount)
+
+  def withdraw(self):
+    amount = float(input("Enter amount to be Withdrawn: "))
+    if self.balance >= amount:
+      self.balance -= amount
+      print("\n You Withdrew:", amount)
+    else:
+      print("\n Insufficient balance  ")
+
+  def display(self):
+    print("\n Net Available Balance=", self.balance)
+
+
+# Driver code
+
+# creating an object of class
+s = Bank_Account()
+
+# Calling functions with that class object
+s.deposit()
+s.withdraw()
+s.display()
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-```python
-def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
-
-# Get input from the user
-num = int(input("Enter a number: "))
-
-# Calculate and print the factorial
-result = factorial(num)
-print("Factorial of", num, "is", result)
+#create an object of the both bowler and batsman classes and call by play function()
+class Player:
+  def play(self):
+   print("The player is playing cricket")
+class Batsman(Player):
+   def play(self):
+    print("The batsman is batting.")
+class Bowler(Player):
+     def play(self):
+      print("The Bowler is bowling.")
+batsman = Batsman()
+bowler=Bowler()
+batsman. play()
+bowler.play()
